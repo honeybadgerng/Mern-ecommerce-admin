38ecf711e95efe77296b1f6973c294f28d06c81b
Honeybadger <rajimoshood200482@gmail.com>
2024-08-06 20:23:09 +0100

    updated shipping rate

--- borcella_admin-master/app/api/checkout/route.ts
+++ borcella_admin-master/app/api/checkout/route.ts
@@ -26,8 +26,8 @@ export async function POST(req: NextRequest) {
         allowed_countries: ["US", "CA"],
       },
       shipping_options: [
-        { shipping_rate: "shr_1MfufhDgraNiyvtnDGef2uwK" },
-        { shipping_rate: "shr_1OpHFHDgraNiyvtnOY4vDjuY" },
+        { shipping_rate: "shr_1PkswVAMwKphXUcX1CGVbd84" },
+        { shipping_rate: "shr_1PksxEAMwKphXUcXCSG15gpk" },
       ],
       line_items: cartItems.map((cartItem: any) => ({
         price_data: {
